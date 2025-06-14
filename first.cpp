0816c0fb071f57b19fa3311e316f8e1f39747775
Milan <milanvchristy420@gmail.com>
2025-06-14 16:09:21 +0000

    Day-01

---
+++ first.cpp
@@ -0,0 +1,26 @@
+// // #include<iostream> //necessary header files for input/output functions 
+// // #define EMPTY
+// // #include<system_error>
+// // #include<span>
+// // #include<cstdio>
+// // #include<thread>
+// // #error "This is an error message"
+// // #ifdef EMPTY 
+// // #warning "this is a warning message" 
+// // using namespace std;
+// #include<iostream>
+// using namespace std;
+
+// //main() function:where the execution of the C++ program begins 
+// // int main(){
+// // #define ABC 1//2134
+// //     // this is the statement that prints "Hello, World! to the console"
+// //     // cout<<"Hello, World!"<<endl;
+// //     cout<<ABC<<" hello world\n";
+// //     return 0;
+// // }
+// int main(int argc,char *argv[]){
+//     cout<<"hello"<<endl;
+//     cout<<"comments: "<<argc;
+//     return 0;
+// }
