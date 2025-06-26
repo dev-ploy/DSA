7fe033eaad9b8aca491ada8815a92525f3f2b9de
milan <milanvchristy420@gmail.com>
2025-06-26 10:50:38 +0530

    Printing the subsets of an array

---
+++ code.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printSubsets(vector<int>&arr,vector<int> &ans,int i){
+    if(i==arr.size()){
+        for(int val:ans){
+            cout<<val<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    ans.push_back(arr[i]);
+    printSubsets(arr,ans,i+1);
+    ans.pop_back();
+    printSubsets(arr,ans,i+1);
+    return ;
+}
+
+
+int main(){
+    vector<int>arr={1,2,3};
+    vector<int>ans;
+    printSubsets(arr,ans,0);
+
+    return 0;
+}
